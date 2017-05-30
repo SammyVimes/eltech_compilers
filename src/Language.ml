b3a1098b461e9384d50e54d3ad4c8b33935e120c
dsv <dsv@sofitlabs.ru>
2017-05-30 19:21:26 +0300

    add functions for parsing

--- src/Language.ml
+++ src/Language.ml
@@ -8,6 +8,7 @@ module Expr =
     | Var   of string
     | Const of int
     | Binop of string * t * t
+    | Call  of string * t list
 
     ostap (
       parse:
@@ -45,6 +46,7 @@ module Expr =
 
       prim:
         n:DECIMAL {Const n}
+      | f:IDENT -"(" args:!(Util.list0 parse) -")" {Call (f, args)}
       | x:IDENT   {Var   x}
       | -"(" parse -")"
     )
@@ -64,10 +66,27 @@ module Stmt =
     | If     of Expr.t * t * t
     | While  of Expr.t * t
     | Repeat of t * Expr.t
+    | Call   of string * Expr.t list
+    | Fun    of string * string list * t
+    | Return of Expr.t
 
     let expr = Expr.parse
 
     ostap (
+      parse: fs:(functions)* m:main {
+          match fs with
+          | [] -> m
+          | fs -> List.fold_right (fun f st -> Seq (f, st)) fs m
+      };
+
+      name: IDENT;
+
+      functions: 
+          %"fun" f:IDENT "(" args:!(Util.list0 name) ")" "begin" body:!(main) "end" { Fun (f, args, body)};
+        
+      main: s:simp d:(-";" (* parse *) main)? {
+	      match d with None -> s | Some d -> Seq (s, d)
+      };
       simp: x:IDENT ":=" e:expr  {Assign (x, e)}
       | %"read"  "(" x:IDENT ")" {Read x}
       | %"write" "(" e:expr  ")" {Write e}
@@ -79,6 +98,7 @@ module Stmt =
       | %"if"        e:!(Expr.parse)
         %"then"      s1:!(parse)
         %"fi"                            {If (e, s1, Skip)}
+      | %"return" e:!(Expr.parse) {Return e}
       | %"while"     e:!(Expr.parse)
         %"do"        s:!(parse)
         %"od"                            {While (e, s)}
@@ -86,8 +106,8 @@ module Stmt =
         %"until"     e:!(Expr.parse)     {Seq (s, While (Binop ("==", e, Const 0), s))}
       | %"for"       i:!(parse) "," n:!(Expr.parse) "," b:!(parse)
         %"do"        a:!(parse)
-        %"od"                            {Seq (i, (While (n, Seq (a, b))))};
-      parse: s:simp ";" d:parse {Seq (s,d)} | simp 
+        %"od"                            {Seq (i, (While (n, Seq (a, b))))}
+      | f:IDENT "(" args:!(Util.list0 Expr.parse) ")" {Call (f, args)} 
     )
 
   end
